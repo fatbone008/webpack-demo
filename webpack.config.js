86b2e92ff5ba8b3a0a4cbbe9f0d0bbd350033fd5
陈贻辉 <yihui.chen@esmart365.com>
2019-07-19 18:02:39 +0800

    Chapter 4 Development unfinished

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     // entry: './src/index.js',
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/print.js'
@@ -14,9 +15,14 @@ module.exports = {
             title: 'Output Management'
         })
     ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './Base'
+    },
     output: {
         // filename: 'bundle.js',
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "/"
     }
 }
