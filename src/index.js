058068b1cdeb456bb2413caf270dab1839baf78b
fatbone008 <165080496@qq.com>
2019-07-27 21:39:53 +0800

    7 Authoring libraries

--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
 import _ from 'lodash';
-// import Print from './print'
+import numRef from './ref'
 
-function component() {
-// function getComponent() {
-    var element = document.createElement('div');
-    // const btn = document.createElement('button');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    // element.onclick = Print.bind(null, 'Hello webpack!');
-    // btn.innerHTML = 'Click me and check the console!';
-    // btn.onclick = printMe;
-
-    // element.appendChild(btn);
-
-    return element;
-
-    // return import(/* webpackChunkName: "lodash" */ 'lodash').then(({default: _}) => {
-    //     var element = document.createElement('div');
-    //
-    //     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    //
-    //     return element;
-    // })
+export function numToWord(num) {
+    return _.reduce(numRef, (accum, ref) => {
+        return ref.num === num ? ref.word : accum;
+    }, '')
 }
 
-// getComponent().then(component => {
-//     document.body.appendChild(component);
-// })
-//
-document.body.appendChild(component());
+export function wordToNum(word) {
+    return _.reduce(numRef, (accum, ref) => {
+        return ref.word === word && word.toLowerCase() ? ref.num : accum;
+    }, -1)
+}
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,19 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-    // entry: './src/index.js',
-    mode: 'development',
     entry: './src/index.js',
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Caching'
-        })
-    ],
-    // devtool: 'inline-source-map',
-    // devServer: {
-    //     contentBase: './Base'
-    // },
     output: {
-        // filename: 'bundle.js',
-        filename: '[name].[contenthash].js',
-        // chunkFilename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist'),
-        // publicPath: "/"
+        filename: 'webpack-numbers.js',
+        library: 'webpackNnumbers',
+        libraryTarget: 'umd'
     },
-    optimization: {
-        moduleIds: 'hashed',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                }
-            }
-        },
-        runtimeChunk: 'single'
+    externals: {
+        lodash: {
+            commonjs: 'lodash',
+            commonjs2: 'lodash',
+            amd: 'lodash',
+            root: '_'
+        }
     }
-}
+};
