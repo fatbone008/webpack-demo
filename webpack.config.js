fb8f2e698655531c31bfb39372b2bfb9837fa30f
fatbone008 <165080496@qq.com>
2019-07-27 21:00:58 +0800

    6 Caching

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     // entry: './src/index.js',
     mode: 'development',
-    entry: {
-        app: './src/index.js',
-        // another: './src/another-module.js'
-    },
-    // plugins: [
-    //     new CleanWebpackPlugin(),
-    //     new HtmlWebpackPlugin({
-    //         title: 'Output Management'
-    //     })
-    // ],
+    entry: './src/index.js',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Caching'
+        })
+    ],
     // devtool: 'inline-source-map',
     // devServer: {
     //     contentBase: './Base'
     // },
     output: {
         // filename: 'bundle.js',
-        filename: '[name].bundle.js',
-        chunkFilename: "[name].bundle.js",
+        filename: '[name].[contenthash].js',
+        // chunkFilename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist'),
         // publicPath: "/"
     },
-    // optimization: {
-    //     //     splitChunks:{
-    //     //         chunks: "all"
-    //     //     }
-    //     // }
+    optimization: {
+        moduleIds: 'hashed',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                }
+            }
+        },
+        runtimeChunk: 'single'
+    }
 }
